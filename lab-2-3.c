26eaa583a7fdde017a1a52f6ecf3a7a63b59d881
Swapnil <144586148+sxenil@users.noreply.github.com>
2023-09-10 20:23:29 +0530

    Update lab-2-3.c

--- lab-2-3.c
+++ lab-2-3.c
@@ -3,21 +3,15 @@ int main (){
 
 printf ("Earnings: \n\n");
 
-float b_pay, hra, dra, mda, it, od, total_earning, total_deduc, net_sal;
+float b_pay, oa, it, total_earning, net_sal;
 
 printf ("Basic pay: ");
 scanf ("%f", &b_pay);
 
-printf ("House rent allowance: ");
-scanf ("%f", &hra);
+printf ("Other allowances: ");
+scanf ("%f", &oa);
 
-printf ("Dearness allowance: ");
-scanf ("%f", &dra);
-
-printf ("Medical allowance: ");
-scanf ("%f", &mda);
-
-total_earning= b_pay + hra + dra + mda;
+total_earning= b_pay + oa;
 
 printf ("\n\nGross salary: %f", total_earning);
 
@@ -26,14 +20,9 @@ printf ("\n\nDeductions: \n\n");
 printf ("Income tax: ");
 scanf ("%f", &it);
 
-printf ("Other deductions: ");
-scanf ("%f", &od);
+printf ("\nGross deduction: %f", it);
 
-total_deduc= it + od;
-
-printf ("\nGross deduction: %f", total_deduc);
-
-net_sal= total_earning - total_deduc;
+net_sal= total_earning - it;
 
 printf ("\n\nNet salary: %f", net_sal);
 
